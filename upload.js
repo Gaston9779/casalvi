809c528630fb2bf6c6e4f4f815f281c903e3b9f1
nicola-viola <nicthareal@gmail.com>
2025-01-08 15:21:09 +0100

    f

--- upload.js
+++ upload.js
@@ -1,19 +1,28 @@
-// upload.js
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
 
-// Percorso assoluto della cartella 'uploads'
-const uploadPath = path.resolve('uploads');
+// Ottieni il percorso assoluto per la cartella uploads nella root del progetto
+const uploadPath = path.resolve(__dirname, 'uploads');
 
+// Verifica se la cartella 'uploads' esiste nella root, se no la crea
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+}
+
+// Configura Multer con il percorso desiderato
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadPath); // Usa il percorso assoluto per 'uploads'
+    // Salva i file nella cartella 'uploads' nella root
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); // Nome del file basato su timestamp
-  },
+    // Imposta il nome del file come timestamp più estensione
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
 });
 
+// Crea l'upload middleware
 const upload = multer({ storage: storage });
 
 export default upload;
